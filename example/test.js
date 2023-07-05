c557a83362c4263586854943ac0d1e2886ae211d
thomasvergne <thomas.vergne.contact@gmail.com>
2023-07-05 12:56:06 +0200

    test: added example file.

---
+++ example/test.js
@@ -0,0 +1,20 @@
+var x = [4, 5, 6],
+    y = 5;
+
+function add(a, b) {
+  return a + b;
+}
+
+var add = function test(a, b) {
+  return a + b;
+}
+
+console.log("teest");
+a += 7
+try {
+  // ...
+} catch (e) {
+  // ...
+} finally {
+  // ...
+}
